a2fa7482724a1ea34154d0823777517cc2bd7df6
Sho Kuniyuki <79742417+shokuniyuki@users.noreply.github.com>
2021-05-14 18:08:57 -0600

    Update mapanimation.js
    changed style from streets to dark

--- mapanimation.js
+++ mapanimation.js
@@ -18,7 +18,7 @@ const busStops = [
   
   let map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
+    style: 'mapbox://styles/mapbox/dark-v10',
     center: [-71.104081, 42.365554],
     zoom: 14,
   });
